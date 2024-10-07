a45e8eaef80a1d6d59d0e59f519a877006478dd2
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-10-07 11:39:16 +0400

    update: redux store &  add usersSlice

--- Social-Media-Feed-App/src/app/store.ts
+++ Social-Media-Feed-App/src/app/store.ts
@@ -1,7 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineSlices, configureStore } from '@reduxjs/toolkit'
+import { postsSlice } from '@/features/posts/postsSlice'
+import { usersSlice } from '@/features/users/usersSlice'
+
+const rootReducer = combineSlices(postsSlice, usersSlice)
 
 export const store = configureStore({
-  reducer: {},
+  reducer: rootReducer,
 })
 
 export type AppStore = typeof store
