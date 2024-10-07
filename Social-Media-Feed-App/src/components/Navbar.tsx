b1a35a2350fbebb21e9bb995b2ec10a6212ee230
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-10-07 11:35:05 +0400

    create: Navbar component

---
+++ Social-Media-Feed-App/src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+export const Navbar = () => (
+  <nav>
+    <section>
+      <h1>Redux Essentials Example</h1>
+      <div className="navContent">
+        <div className="navLinks">
+          <Link to="/">Posts</Link>
+        </div>
+      </div>
+    </section>
+  </nav>
+)
