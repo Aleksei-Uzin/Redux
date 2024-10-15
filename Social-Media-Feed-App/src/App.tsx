91406eab2716705bf994f9da4b365bd8ca02d132
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-10-15 12:43:08 +0400

    update: store

--- Social-Media-Feed-App/src/App.tsx
+++ Social-Media-Feed-App/src/App.tsx
@@ -1,8 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { PostsMainPage } from './features/posts/PostsMainPage'
 import { SinglePostPage } from './features/posts/SinglePostPage'
 import { EditPostForm } from './features/posts/EditPostForm'
 import { Navbar } from './components/Navbar'
+import { useAppSelector } from './app/hooks'
+import { selectCurrentUsername } from './features/auth/authSlice'
+import { LoginPage } from './features/auth/LoginPage'
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const username = useAppSelector(selectCurrentUsername)
+
+  if (!username) {
+    return <Navigate to="/" replace />
+  }
+
+  return children
+}
 
 function App() {
   return (
@@ -10,9 +23,19 @@ function App() {
       <Navbar />
       <div className="App">
         <Routes>
-          <Route path="/" element={<PostsMainPage />} />
-          <Route path="/posts/:postId" element={<SinglePostPage />} />
-          <Route path="/editPost/:postId" element={<EditPostForm />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/*"
+            element={
+              <ProtectedRoute>
+                <Routes>
+                  <Route path="/posts" element={<PostsMainPage />} />
+                  <Route path="/posts/:postId" element={<SinglePostPage />} />
+                  <Route path="/editPost/:postId" element={<EditPostForm />} />
+                </Routes>
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </BrowserRouter>
--- Social-Media-Feed-App/src/app/store.ts
+++ Social-Media-Feed-App/src/app/store.ts
@@ -1,8 +1,9 @@
 import { combineSlices, configureStore } from '@reduxjs/toolkit'
 import { postsSlice } from '@/features/posts/postsSlice'
 import { usersSlice } from '@/features/users/usersSlice'
+import { authSlice } from '@/features/auth/authSlice'
 
-const rootReducer = combineSlices(postsSlice, usersSlice)
+const rootReducer = combineSlices(postsSlice, usersSlice, authSlice)
 
 export const store = configureStore({
   reducer: rootReducer,
