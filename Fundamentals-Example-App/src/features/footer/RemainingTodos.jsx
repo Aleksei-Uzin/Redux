b2e0f6ff0675307c944787cddc1a4ed229471f33
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-10-30 19:09:52 +0400

    create: UI components

---
+++ Fundamentals-Example-App/src/features/footer/RemainingTodos.jsx
@@ -0,0 +1,10 @@
+export const RemainingTodos = ({ count }) => {
+  const suffix = count === 1 ? '' : 's'
+
+  return (
+    <div className="todo-count">
+      <h5>Remaining Todos</h5>
+      <strong>{count}</strong> item{suffix} left
+    </div>
+  )
+}
