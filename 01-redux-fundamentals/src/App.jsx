5870fe60750c0c10b5e208bad6e4880b7ff8dea0
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-12-05 19:46:55 +0400

    add index.js and App.jsx

---
+++ 01-redux-fundamentals/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from './features/header/Header'
+import Footer from './features/footer/Footer'
+import TodoList from './features/todos/TodoList'
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <section className="medium-container">
+          <h2>Todos</h2>
+          <div className="todoapp">
+            <Header />
+            <TodoList />
+            <Footer />
+          </div>
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default App
